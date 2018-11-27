a0c367512d35a76aefafffb55924f88bba7eefd7
陈饶中 <@G05X0R88UIQ92SL>
2018-11-27 22:29:04 +0800

    第二次尝试

--- test/test/test.cpp
+++ test/test/test.cpp
@@ -3,11 +3,17 @@
 
 #include "stdafx.h"
 #include<iostream>
+#include<graphics.h>
 using namespace std;
 
 
 int main()
 {	
+	int gdrive = DETECT, gmode;
+	initgraph(&gdrive, &gmode, "");
+	line(0, 0, 100, 100);
+	getchar();
+	closegraph();
 	cout << "hello world" << endl;
     return 0;
 }
