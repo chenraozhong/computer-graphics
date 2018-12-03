f64ed549e331693c81ec9803de26198887964894
陈饶中 <@G05X0R88UIQ92SL>
2018-12-03 16:48:06 +0800

    在交点处存在bug

--- test/test/test.cpp
+++ test/test/test.cpp
@@ -29,7 +29,8 @@ void DDA(int x1, int y1, int x2, int y2)
 当差值过小的时候，会出现刚画的线在下一秒就会被清除的情况，这是一个bug
 */
 void draw(int *p, int n) {
-	drawpoly(n + 1, p);
+	for (int i = 0; i < 2 * n; i += 2)
+		line(p[i], p[i + 1], p[i + 2], p[i + 3]);
 	int xmin, xmax, ymin, ymax;
 	xmin = xmax = p[0];
 	ymin = ymax = p[1];
@@ -40,31 +41,34 @@ void draw(int *p, int n) {
 		if (p[i + 1] < ymin) ymin = p[i + 1];
 		if (p[i + 1] > ymax) ymax = p[i + 1];
 	}
-	int x0, y0, x1, y1;
+	float x0, y0, x1, y1;
 	for (int i = 0; i < 2 * n; i += 2) {
-		x0 = p[i];
-		y0 = p[i + 1];
-		x1 = p[i + 2];
-		y1 = p[i + 3];
+		x0 = (float)p[i];
+		y0 = (float)p[i + 1];
+		x1 = (float)p[i + 2];
+		y1 = (float)p[i + 3];
 		if (x0 > x1) {
-			int buff = x0;
-			int buff1 = y0;
+			float buff = x0;
+			float buff1 = y0;
 			x0 = x1;
 			y0 = y1;
 			x1 = buff;
 			y1 = buff1;
 		}
 
-		int dx = x1 - x0;//之前是x0-x1，是存在错误的
-		int dy = y1 - y0;
-		int espl = (abs(dx) > abs(dy)) ? abs(dx) : abs(dy);//espl应该为正值，存在错误
-		float x = x0, y = y0;
-		float xince = ((float)dx / (float)espl);//必须加上float，否则会出错
-		float yince = ((float)dy / (float)espl);
+		float dx = x1 - x0;
+		float dy = y1 - y0;
+
+		float espl = (abs(dx) > abs(dy)) ? abs(dx) : abs(dy);
+		float x = (float)x0, y = (float)y0;
+		float xince = dx /espl;
+		float yince = dy / espl;
+
+
 		int lasty = -100;//用来记录上一个点，以防止重叠造成误擦或误画
-		for (; abs(y - (float)y1) >= 0.5;) {
+		for (; abs(y - y1) >= 0.001;) {
 			if (lasty !=int( y+0.5)) {
-				float xx = (float)x;
+				float xx = x;
 				int lastx = -100;
 				for (; xx >= xmin&&xx <= xmax; xx += xince) {//xx为int类型，所以加上0.5之后，值是不会改变的
 					if (lastx != int(xx + 0.5)) {
@@ -74,31 +78,38 @@ void draw(int *p, int n) {
 					}
 					lastx = int(xx + 0.5);
 				}
-				
+			//	for (int i = 0; i < 10000000; i++);
 			}
 			lasty = int(y+0.5);
 			y += yince;
 			x += xince;
 			//	line(x, y, 200, 100);
 		}
-		for (int i = 0; i < 1000000000; i++);
+	//	for (int i = 0; i < 100000000; i++);
 	}
 	//line(0, 0, 100, 100);
 }
 int main()
 {
 	int gdrive = DETECT, gmode;
+	int n;
+	cout << "请输入多边形的顶点个数" << endl;
+	cin >> n;
+	cout << "请输入多边形个顶点的坐标" << endl;
+	int q[20];
+	for (int i = 0; i < 2 * n; i++)
+		cin >> q[i];
+	int jj = q[3];
+	q[2 * n] = q[0];
+	q[2 * n + 1] = q[1];
+	getchar();
 	initgraph(&gdrive, &gmode, "");
 	setcolor(RED);
 	setbkcolor(WHITE);
-	int p[10] = { 10,260,100,160,300,10,210,100,10,260 };
-	draw(p, 4);
-	for (int i = 0; i < 5; i++)
-		cout << "dfad" << endl;
+	draw(q, n);
 	getchar();
 	closegraph();
-	cout << "chen" << endl;
-	cout << "hello world" << endl;
+	cout << "处理完毕" << endl;
 	return 0;
 }
 
