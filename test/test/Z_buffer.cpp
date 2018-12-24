e4b45a9e47921ac11d3dbea3fa4da6e298143db4
陈饶中 <@G05X0R88UIQ92SL>
2018-12-24 20:48:18 +0800

    Z_BUFFER算法没有完成

--- test/test/Z_buffer.cpp
+++ test/test/Z_buffer.cpp
@@ -3,13 +3,20 @@
 #include<graphics.h>
 #include<iostream>
 #include<vector>
+#include<map>
+#include<algorithm>
 using namespace std;
 
-
+#if 1
 typedef struct node {
 	int x;//边界x
 	int y;//边界y
 }Node;
+
+typedef struct Border {
+	int xr;
+	int xl;
+}Border;
 
 //求出平面方程的系数
 void get_coe(vector<int> &A, vector<POINT > ptr, vector<int> z) {
@@ -18,7 +25,6 @@ void get_coe(vector<int> &A, vector<POINT > ptr, vector<int> z) {
 	A[2] = (ptr[1].x - ptr[0].x)*(ptr[2].y - ptr[0].y) - (ptr[2].x - ptr[0].x)*(ptr[1].y - ptr[0].y);
 	A[3] = -(A[0] * ptr[0].x + A[1] * ptr[0].y + A[2] * z[0]);
 }
-
 
 //计算出每个最大的y值和最大的x值
 Node* getmax(vector<POINT> ptr) {
@@ -49,6 +55,58 @@ Node* getmin(vector<POINT > ptr) {
 	return n;
 }
 
+void get_eidge_point(map<int, int> eidge, POINT point1,POINT point2) {
+	float k = (float)(point1.y - point2.y) / (float)(point1.x - point2.x);
+	if (point1.x > point2.x) {
+		POINT buff;
+		buff = point1;
+		point2 = buff;
+		point1 = point2;
+	}
+	float detx, dety;
+	if (k > 1) {
+		detx = 1 / k;
+		dety = 1;
+	}
+	else {
+		detx = 1;
+		dety = k;
+	}
+	float x = point1.x;
+	float y = point1.y;
+	while (true) {
+		eidge[(int)(y + 0.5)] = (int)(x + 0.5);
+		x += detx;
+		y += dety;
+		if (x >= point2.x) break;
+	}
+}
+
+void get_Eidge(vector<map<int, Border> > &Eidge, vector<vector<POINT > >ptr) {
+	for (int i = 0; i < ptr.size(); i++) {
+		//eidge1、eidge2、eidge3分别存储多边形上每一条边上的点坐标
+		vector<map<int, int>> eidge;
+		get_eidge_point(eidge[0], ptr[i][0], ptr[i][1]);
+		get_eidge_point(eidge[1], ptr[i][1], ptr[i][2]);
+		get_eidge_point(eidge[2], ptr[i][2], ptr[i][0]);
+		int y[3];
+		for (int pp = 0; pp < 3; pp++)
+			y[pp] = ptr[i][pp].y;
+		sort(y[0],y[2]);
+		for (int pp = 0; pp < 2; pp++) {
+			int count = 2;//选出含y值为y[pp]的边
+			vector<int> e;
+			for (int j = 0; j < 3; j++) {
+				if (eidge[j].find(y[pp]) != eidge[j].end() && count >= 0) {
+					count--;
+					e.push_back(j);
+				}
+			}
+//////////////////////////////////////////////////////////////////////////////////////////////
+		}
+	}
+}
+
 int main() {
 	COLORREF color[4] = { RED,YELLOW,BLUE,GREEN };
 	//各面的顶点的x和y坐标
@@ -67,6 +125,11 @@ int main() {
 
 	vector<Node *> min;
 	vector<Node *> max;
+	/*
+		使用map来存储每一个多边形在一条y轴上的两个x的边界值，用于在更改Z_BUFFER中的z值使用。	
+	*/
+	vector<map<int, Border> > Eidge;
+	get_Eidge(Eidge,ptr);
 
 	for (int i = 0; i < 4; i++) {
 		get_coe(A[i], ptr[i], z[i]);
@@ -84,9 +147,12 @@ int main() {
 		if (buff_maxx < max[i]->x) buff_maxx = max[i]->x;
 		if (buff_maxy < max[i]->y) buff_maxy = max[i]->y;
 	}
-	
+	vector<vector <COLORREF > > color_buffer;
+	vector<vector<float > > z_buffer;
+
 	int gdrive = DETECT, gmode;
 	cout << &gdrive << endl;
 	return 0;
 	
 }
+#endif
--- test/test/main.cpp
+++ test/test/main.cpp
@@ -2,14 +2,21 @@
 #include "stdafx.h"
 #include<iostream>
 #include<graphics.h>
-#include"第三次实验\斜二测.h"
 #include<time.h>
+#include<map>
 using namespace std;
 
 #if 0
 int main()
 {
-	
+	map<int, int> e;
+	e[1] = 0;
+	e[2] = 8;
+	e[3] = 4;
+	map<int, int>::iterator item;
+
+	item = e.lower_bound(2);
+	cout <<item->second << endl;
 	return 0;
 }
 #endif
